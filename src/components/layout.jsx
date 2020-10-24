b37999b248dc0cbc23c76eb7d1e2827ad9de6e23
Erickowski <daniel_perez_03081995@hotmail.com>
2020-10-24 17:22:54 -0500

    Agregando Helmet

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,9 +1,24 @@
 import React from "react"
+import Helmet from "react-helmet"
+
 import Header from "./header"
 
 const Layout = ({ children }) => {
   return (
     <>
+      <Helmet>
+        <title>Gatsby Hotel</title>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css"
+          integrity="sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w=="
+          crossOrigin="anonymous"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&family=Roboto:wght@400;700&display=swap"
+          rel="stylesheet"
+        />
+      </Helmet>
       <Header />
       {children}
     </>
